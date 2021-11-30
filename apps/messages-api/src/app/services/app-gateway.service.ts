b189c1ee4be84b154ef4552568c5ffba8ee06af1
Dan Radu <xdanradu@yahoo.com>
2021-11-30 11:17:09 +0200

    up

--- apps/messages-api/src/app/services/app-gateway.service.ts
+++ apps/messages-api/src/app/services/app-gateway.service.ts
@@ -4,7 +4,7 @@ import {
   OnGatewayConnection,
   OnGatewayDisconnect,
   OnGatewayInit,
-  SubscribeMessage, MessageBody, WsResponse
+  SubscribeMessage
 } from '@nestjs/websockets';
 import { Socket, Server } from 'socket.io';
 import { Logger } from '@nestjs/common';
@@ -16,29 +16,43 @@ export class AppGateway
   @WebSocketServer() server: Server;
 
   private logger: Logger = new Logger('AppGateway');
-  private users = 0;
+  private connectedClients = [];
 
   @SubscribeMessage('msgToServer')
-  handleMessage(client: Socket, message: string): void {
-    //console.log('chat');
-    console.log(message);
-    this.server.emit('msgToClient', 'received'+message.channel);
-    // return { event: 'msgToClient', data: message };
+  handleMessage(client: Socket, message: any): void {
+    this.logger.log('Received message from client id: '+client.id);
+    this.logger.log(message);
+    this.server.emit('msgToClient', 'broadcast message to all clients <'+message.channel+'> triggered by client id: '+client.id);
+    this.server.emit('msgToClient', 'online clients: ' + this.connectedClients.length);
+    client.emit('msgToClient', 'only one for '+ client.id);
   }
 
   afterInit(server: Server) {
-    this.logger.log('Init');
+    this.logger.log('Initialize the ws server');
   }
 
   handleDisconnect(client: Socket) {
-    this.users--;
-    this.server.emit('users', this.users);
-    this.logger.log(`Client disconnected: ${client.id}`);
+    this.removeFromConnectedClients(client.id);
+    this.logger.log(`Client id <${client.id}> disconnected`);
+    this.logger.log('Online users count: '+this.connectedClients.length);
+  }
+
+  removeFromConnectedClients(id: string) {
+    let index = null;
+    for (let i=0;i<this.connectedClients.length; i++) {
+      if (this.connectedClients[i].id === id) {
+        index = i;
+      }
+    }
+    if (index !== null) {
+      this.connectedClients.splice(index, 1);
+      this.logger.log(`Index ${index} removed from the connected client's list`);
+    }
   }
 
   handleConnection(client: Socket, ...args: any[]) {
-    this.users++;
-    this.logger.log(`Client connected ${client.id}`);
-    this.server.emit(`msgToClient`, `received client_id: ${client.id}`);
+    this.connectedClients.push(client);
+    this.logger.log(`Client id <${client.id}> connected`);
+    this.server.emit(`msgToClient`, `Connected on id: ${client.id}`);
   }
 }
